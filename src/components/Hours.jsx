033e6e86f7a1071698e59e6a2d1e52d6d3e65430
Giga Tokhishvili <toxishvili123@gmail.com>
2024-03-10 14:29:07 +0400

    css file error

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import hourStyles from '../styles/Hours.module.css'
+import React, { useEffect, useState } from 'react';
+import HourStyles from '../styles/Hours.module.css';
 import { nanoid } from 'nanoid';
 
 function Hours({header, description, shiftType, totalHours}) {
@@ -48,27 +48,27 @@ function Hours({header, description, shiftType, totalHours}) {
 
     return (
     <section>
-        <div onClick={() => setModal(prev => !prev)} className={hourStyles.modalDiv}>
+        <div onClick={() => setModal(prev => !prev)} className={HourStyles.modalDiv}>
             <h1>{header}</h1>
-            <span className={modal ? hourStyles.modalBtnUp : hourStyles.modalBtnDown}>﹀</span>
+            <span className={modal ? HourStyles.modalBtnUp : HourStyles.modalBtnDown}>﹀</span>
         </div>
-        <div className={modal ? hourStyles.openModal : hourStyles.closeModal}>
+        <div className={modal ? HourStyles.openModal : HourStyles.closeModal}>
             <p>{description}</p>
         </div>
 
-        <div className={hourStyles.hoursDiv}>
-            <button className={hourStyles.button38} onClick={handleClick} >-16</button>
-            <button className={hourStyles.button38} onClick={handleClick} >-4</button>
+        <div className={HourStyles.hoursDiv}>
+            <button className={HourStyles.button38} onClick={handleClick} >-16</button>
+            <button className={HourStyles.button38} onClick={handleClick} >-4</button>
             <input 
                 value={hours} 
                 onChange={handleHourChange}
                 type="number" 
                 placeholder='Hours worked'
                 id={nanoid()}
-                className={hourStyles.hoursInput}
+                className={HourStyles.hoursInput}
             />
-            <button className={hourStyles.button38} onClick={handleClick} >+4</button>
-            <button className={hourStyles.button38} onClick={handleClick} >+16</button>
+            <button className={HourStyles.button38} onClick={handleClick} >+4</button>
+            <button className={HourStyles.button38} onClick={handleClick} >+16</button>
         </div>
     </section>
     )
